6538f9736ceefe0cefefa4c11d765499a3583ab7
TheGorlox <44106936+TheGorlox@users.noreply.github.com>
2018-10-13 19:20:18 -0500

    websiteupdate

--- TheBeets/Website/venv/main.py
+++ TheBeets/Website/venv/main.py
@@ -5,17 +5,24 @@ app = Flask(__name__)
 
 
 # @ signifies a decorator -way to wrap a function and modigying its behavior
+@app.route('/<user>/home')
 @app.route("/")
-def index():
+def index(user=None):
     return render_template('home.html')
+@app.route('/<user>/article')
 @app.route('/article')
-def article():
+def article(user=None):
     #Outputs
+    username="Username"
     headline="Headline"
     credibility=80
     bias=58
     blank=11
-    return render_template('article.html', credibility=credibility, bias=bias, blank=blank, headline=headline)
+    return render_template('article.html', credibility=credibility, bias=bias, blank=blank, headline=headline, user=user, username=username)
+
+@app.route('/<user>/history')
+def history(user=None):
+    return render_template('_history.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
